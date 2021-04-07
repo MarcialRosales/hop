acbe0c9be9c0b26154357e633334c4898284c71c
Marcial Rosales <mrosales@pivotal.io>
2021-04-07 09:20:38 +0200

    Add test that reproduces the encoding issue with UriBuilder from Spring

---
+++ src/test/java/com/rabbitmq/http/client/EncodingTest.java
@@ -0,0 +1,46 @@
+package com.rabbitmq.http.client;
+
+import org.apache.http.client.utils.URIBuilder;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class EncodingTest {
+    String queryParameterValue = "^outbound-ack|msg-.+$";
+
+    @Test
+    void uriEncodeOfSpecialCharactersWithSpringUriBuilder() {
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("name", queryParameterValue);
+
+        URI uri = factory
+                .uriString("/exchanges")
+                .queryParams(map)
+                .build();
+        assertEquals("/exchanges?name=%5Eoutbound-ack%7Cmsg-.%2B%24", uri.toASCIIString());
+    }
+
+    @Test
+    void uriEncodeOfSpecialCharactersWithApacheURIBuilder() throws URISyntaxException {
+        URI uri = new URIBuilder()
+                .setPath("/exchanges")
+                .addParameter("name", queryParameterValue)
+                .build();
+        assertEquals("/exchanges?name=%5Eoutbound-ack%7Cmsg-.%2B%24", uri.toASCIIString());
+    }
+}
